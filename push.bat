fac5c44f918a166130fb766110a3ff7f9de69fd5
svgorin <svgorin@gmail.com>
2026-08-07 14:43:51 +0300

    subs.js

--- push.bat
+++ push.bat
@@ -1,1 +1,4 @@
-git add . &git commit -m "subs.js"&git push
+git add .
+git commit -m "subs.js"
+git push
+"C:\Program Files\PuTTY\plink.exe" -batch -hostkey "ssh-ed25519 255 SHA256:3oWGtg6CjATOylTPZxrb3nl7jPmenzqpJGJJj4gyFlo" -i "G:\My Drive\VPS\timeweb\priv.ppk" root@timeweb.svgrn.work "bash /opt/svgwork-site/deploy_sub_server.sh"
