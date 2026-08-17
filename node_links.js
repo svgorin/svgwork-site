b95e0df343d118c8923dd8cecb406a875f51fcc2
svgorin <svgorin@gmail.com>
2026-08-17 16:26:17 +0300

    subs.js

--- node_links.js
+++ node_links.js
@@ -9,6 +9,7 @@ module.exports = [
   "trojan://d1dcec4a-9f55-4073-b068-b1073d2f583d@md.svgrn.work:443?type=grpc&security=tls&serviceName=%2F22899%2Fo0GdKCj7h3&authority=md.svgrn.work&sni=md.svgrn.work#md0708trojan",
 
   "vless://57825bae-1d76-4be6-81ac-944734401557@seltel.svgrn.work:51732?encryption=none&type=tcp&security=reality&fp=chrome&sni=dl.google.com&pbk=_n5BSv-BFgNNg2Qku6Fy9Imn2ekOYVqllyQAUu7KHWw&sid=809ee562267e7965#SELTEL-REALITY",
+  "vless://f3ac9b0f-c194-4e3f-ac80-17957eac3dff@seltel.svgrn.work:9443?encryption=none&type=tcp&security=tls&flow=xtls-rprx-vision&sni=seltel.svgrn.work#seltel9443",
   "vless://78fb87ba-2ae1-4bb2-8ea4-096e623cec96@timeweb.svgrn.work:443?type=xhttp&encryption=none&path=%2FBxuJlQBYBs&host=timeweb.svgrn.work&sni=timeweb.svgrn.work&mode=packet-up&x_padding_bytes=100-1000&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&security=tls#TimewebMSK",
   "vless://266185b0-844a-4d00-af3a-384660196d6f@ilpt2.svgrn.work:2053?fp=chrome&sni=ilpt2.svgrn.work&type=ws&path=%2FzG8sPvQe&host=ilpt2.svgrn.work&security=tls#url0327IL"
 ];
