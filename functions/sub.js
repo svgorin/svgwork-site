15b0506796af788ef2b5b4ca265b271c4af2d9a3
svgorin <svgorin@gmail.com>
2026-09-12 21:52:36 +0300

    Update CF subscription with current nodes and keys

--- functions/sub.js
+++ functions/sub.js
@@ -17,17 +17,24 @@ export async function onRequest(context) {
 
   // 2. Define your node URLs (one per line)
   const nodeLinks = [
-    // ilpt2xhttp-firstx
-    "vless://4e1e74bb-df9d-43ac-945e-890497369d7a@ilpt2.svgrn.work:443?type=xhttp&encryption=none&path=%2Fo8tXBUV0Qe&host=&mode=packet-up&security=tls#ilpt2-xhttp",
-    // ilpt2 latest iphone/mtsMsk bypass
-    "vless://266185b0-844a-4d00-af3a-384660196d6f@ilpt2.svgrn.work:2053?fp=chrome&sni=ilpt2.svgrn.work&type=ws&path=%2FzG8sPvQe&host=ilpt2.svgrn.work&security=tls#ilpt2-04-07-2026",
-    // MyREALITYseltel
-    "vless://57825bae-1d76-4be6-81ac-944734401557@seltel.svgrn.work:51732?security=reality&encryption=none&pbk=I_xfb96Z2i5Iz_HoSlD5PuxPNOP6AU33Qz5JR22xcyg&headerType=none&fp=chrome&type=tcp&sni=www.yandex.ru&sid=79f3f8d1cba04c49#MyREALITY-seltel",
-    "vless://57825bae-1d76-4be6-81ac-944734401557@seltel.svgrn.work:51732?encryption=none&security=reality&sni=www.yandex.ru&fp=chrome&pbk=I_xfb96Z2i5Iz_HoSlD5PuxPNOP6AU33Qz5JR22xcyk&sid=79f3f8d1cba04c49&type=tcp#SELTEL-REALITY",
-    // Timeweb
-    "vless://78fb87ba-2ae1-4bb2-8ea4-096e623cec96@timeweb.svgrn.work:443?type=xhttp&encryption=none&path=%2FBxuJlQBYBs&host=timeweb.svgrn.work&mode=packet-up&x_padding_bytes=100-1000&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&security=tls#TimewebMSK",
-    // ILPT2-reality (SNI updated to www.microsoft.com to bypass Google throttling)
-    "vless://4e1e74bb-df9d-43ac-945e-890497369d7a@185.28.152.61:443?security=reality&encryption=none&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&pbk=Pg9mXzrcCaW6lEYs9IztnM8H5bj_i0OcFIG2QIqNLxs&sni=www.microsoft.com&sid=84e052553543594e#ilpt2-reality"
+    "vless://d1dcec4a-9f55-4073-b068-b1073d2f583d@veesp-2.svgrn.work:443?security=reality&encryption=none&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&pbk=Y_YpgteBD1JQJBjtrESolnk8mbrVaY4mb6nuXi2ztSU&sni=veesp-2.svgrn.work&sid=de8b8bafaa0b4acb#LV0808reality",
+    "vless://d1dcec4a-9f55-4073-b068-b1073d2f583d@veesp.svgrn.work:443?type=ws&security=tls&path=%2F13117%2F8eJYdnO212&host=veesp.svgrn.work&sni=veesp.svgrn.work&encryption=none#LV0808ws",
+    "trojan://d1dcec4a-9f55-4073-b068-b1073d2f583d@veesp.svgrn.work:443?type=grpc&security=tls&serviceName=%2F36395%2FD86ifozy18&authority=veesp.svgrn.work&sni=veesp.svgrn.work#LV0808trojan",
+    "hysteria2://d1dcec4a-9f55-4073-b068-b1073d2f583d@veesp.svgrn.work:443?sni=veesp.svgrn.work&obfs=salamander&obfs-password=svgobfshypass#LV0808hy2",
+    "vless://d1dcec4a-9f55-4073-b068-b1073d2f583d@md-2.svgrn.work:443?security=reality&encryption=none&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&pbk=Pf-AYuI9dT31I7ZlI5NVJISzTw5ahG-jw-iMxjSsSTk&sni=md-2.svgrn.work&sid=b2e9cd1f5a684703#md0708reality",
+    "vless://d1dcec4a-9f55-4073-b068-b1073d2f583d@md.svgrn.work:443?type=ws&security=tls&path=%2F46469%2FUKxNbRsRyJ&host=md.svgrn.work&sni=md.svgrn.work&encryption=none#md0708ws",
+    "vless://d1dcec4a-9f55-4073-b068-b1073d2f583d@md.svgrn.work:443?type=xhttp&encryption=none&path=%2FVEQodnEXkv&host=md.svgrn.work&sni=md.svgrn.work&mode=packet-up&x_padding_bytes=100-1000&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&security=tls#md0708xhttp",
+    "trojan://d1dcec4a-9f55-4073-b068-b1073d2f583d@md.svgrn.work:443?type=grpc&security=tls&serviceName=%2F22899%2Fo0GdKCj7h3&authority=md.svgrn.work&sni=md.svgrn.work#md0708trojan",
+    "vless://57825bae-1d76-4be6-81ac-944734401557@seltel.svgrn.work:51732?encryption=none&type=tcp&security=reality&fp=chrome&sni=dl.google.com&pbk=_n5BSv-BFgNNg2Qku6Fy9Imn2ekOYVqllyQAUu7KHWw&sid=809ee562267e7965#SELTEL-REALITY",
+    "vless://f3ac9b0f-c194-4e3f-ac80-17957eac3dff@seltel.svgrn.work:9443?encryption=none&type=tcp&security=tls&flow=xtls-rprx-vision&sni=seltel.svgrn.work#seltel9443",
+    "vless://a4f61ab4-6b7d-423e-8af7-bb2423be783b@timeweb2.svgrn.work:443?security=reality&encryption=none&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&pbk=6N6BHYvPT9FgWrW74DexzmqJoN_-CpsUQlJW97hL3xI&sni=timeweb2.svgrn.work&sid=d2653f6888fbc20d#Timeweb-Reality",
+    "vless://78fb87ba-2ae1-4bb2-8ea4-096e623cec96@timeweb.svgrn.work:443?type=xhttp&encryption=none&path=%2FBxuJlQBYBs&host=timeweb.svgrn.work&sni=timeweb.svgrn.work&mode=packet-up&x_padding_bytes=100-1000&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&security=tls#TimewebMSK",
+    "vless://266185b0-844a-4d00-af3a-384660196d6f@ilpt2.svgrn.work:2053?fp=chrome&sni=ilpt2.svgrn.work&type=ws&path=%2FzG8sPvQe&host=ilpt2.svgrn.work&security=tls#url0327IL",
+    "vless://4e1f7a28-98e6-42d7-a5c3-8f0d8a5c1b92@aeza.svgrn.work:443?security=reality&encryption=none&pbk=GnT0D3D34FHXL5FOxurs7fjW-9_vvkuAP3lsROIU4T8&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=aeza2.svgrn.work&sid=f8ccb76bb6eea439#aeza-reality",
+    "trojan://4e1f7a28-98e6-42d7-a5c3-8f0d8a5c1b92@aeza.svgrn.work:443?type=grpc&security=tls&serviceName=%2F37926%2FUtWbtCyfKb&authority=aeza.svgrn.work&sni=aeza.svgrn.work#aeza-msk-trojan",
+    "vless://4e1f7a28-98e6-42d7-a5c3-8f0d8a5c1b92@aeza.svgrn.work:443?type=xhttp&encryption=none&path=%2FxqTuWcHfA6&host=aeza.svgrn.work&sni=aeza.svgrn.work&mode=packet-up&x_padding_bytes=100-1000&extra=%7B%22mode%22%3A%22packet-up%22%2C%22xPaddingBytes%22%3A%22100-1000%22%7D&security=tls#aeza-msk-xhttp",
+    "wireguard://aMCrsVcZEmPqD2/d8hDlDcc8hZR1ww+owO2qPYOXX2k=@timeweb.svgrn.work:42720?address=10.0.0.2%2F32&publickey=xyMgj%2BaHp1HH6NnFQaCLF0OcQ60fJGEXpW7JRly%2F8Vo%3D&dns=1.1.1.1%2C+1.0.0.1&mtu=1420#timeweb-wg",
+    "wireguard://aMCrsVcZEmPqD2/d8hDlDcc8hZR1ww+owO2qPYOXX2k=@aeza.svgrn.work:42720?address=10.0.0.2%2F32&publickey=xyMgj%2BaHp1HH6NnFQaCLF0OcQ60fJGEXpW7JRly%2F8Vo%3D&dns=1.1.1.1%2C+1.0.0.1&mtu=1420#aeza-wg"
   ];
 
   const userAgent = (request.headers.get("User-Agent") || "").toLowerCase();
